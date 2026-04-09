8040faf2533ee6353e5c2aeef3b2629533011755
Jose Angel <jangel.romero@gmail.com>
2026-04-10 01:29:51 +0200

    » useForecastCurrent.ts useForecastDaily.ts

--- src/OpenMeteo/current/useForecastCurrent.ts
+++ src/OpenMeteo/current/useForecastCurrent.ts
@@ -8,17 +8,29 @@ import transformer from "./transformer.js";
  * Custom hook to fetch current weather data from OpenMeteo API.
  */
 export function useForecastCurrent(
-  lat: number,
-  lon: number,
+  lat: number | false | null,
+  lon: number | false | null,
   refreshIntervalMin: number = 0): ForecastData
   {
-  // Validate --
-  if (lat == null || lon == null) {
-    throw new Error("useForecastCurrent: invalid coordinates");
+
+  // Lógica de validación ---
+  const shouldFetch = lat !== false && lon !== false;
+  const hasMissingData = lat === null || lon === null;
+
+    // Aviso en consola si hay un NULL inesperado ---
+  if (hasMissingData) {
+    console.warn(
+      `[useForecastCurrent]: Petición abortada por datos faltantes (null/undefined).`,
+      { lat, lon }
+    );
   }
 
+  // Definición de la KEY para SWR ---
+  const apiUrl = shouldFetch && !hasMissingData
+  ? getPath(lat, lon, fetchParams)
+  : null;
+
   // Fetch ---
-  const apiUrl = getPath(lat, lon, fetchParams);
   const { data, error, isLoading } = useSWR(apiUrl, fetcher, {
     refreshInterval: refreshIntervalMin * 60 * 1000,
   });
