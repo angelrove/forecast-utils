7463763a7d253ff3a5fa6b604abbefa8b95f8a3b
Jose Angel <jangel.romero@gmail.com>
2026-04-24 13:34:33 +0200

    » fetchParams.js useForecastDailyAll.ts

---
+++ src/OpenMeteo/daily/useForecastDailyAll.ts
@@ -0,0 +1,53 @@
+import useSWR from "swr";
+import { fetcher, getPath } from "../conf.js";
+import { fetchParams as fetchParamsHourly } from "../hourly/fetchParams.js";
+import type { ForecastData } from "../types.js";
+import { fetchParams as fetchParamsDaily } from "./fetchParams.js";
+import transformer from "./transformer.js";
+
+/**
+ * Custom hook to fetch daily forecast (10 days) data from OpenMeteo API.
+ * https://api.open-meteo.com/v1/forecast?timezone=auto&latitude=36.6644363&longitude=-4.5108962&forecast_days=10&daily=weathercode
+ */
+export function useForecastDailyAll(
+  lat: number | false | null,
+  lon: number | false | null,
+  // refreshIntervalMin: number = 0
+): ForecastData
+  {
+
+  // Lógica de validación ---
+  const shouldFetch = lat !== false && lon !== false;
+  const hasMissingData = lat === null || lon === null;
+
+    // Aviso en consola si hay un NULL inesperado ---
+  if (hasMissingData) {
+    console.warn(
+      `[useForecastDayly]: Petición abortada por datos faltantes (null/undefined).`,
+      { lat, lon }
+    );
+  }
+
+  // Definición de la KEY para SWR ---
+  const apiUrl = shouldFetch && !hasMissingData
+  ? getPath(lat, lon, fetchParamsDaily + "&" + fetchParamsHourly)
+  : null;
+
+  // Fetch --
+  // Cache
+  const cacheOps = {
+    // Durante este tiempo, SWR considera que el dato que tiene es lo suficientemente nuevo y no disparará el fetcher.
+    dedupingInterval: (15 * 60 * 1000), // 15 minutos en milisegundos
+  };
+
+  // Fetch
+  const { data, error, isLoading } = useSWR(apiUrl, fetcher, cacheOps);
+
+  // Return --
+  return {
+    data: data ? transformer(data) : null,
+    apiUrl,
+    isLoading,
+    isError: error,
+  };
+}
--- src/OpenMeteo/hourly/fetchParams.js
+++ src/OpenMeteo/hourly/fetchParams.js
@@ -1,5 +1,6 @@
 export const fetchParams =
   "hourly=" +
+  "is_day," +
   "weathercode," +
   "temperature_2m," +
   "windspeed_10m," +
@@ -7,8 +8,7 @@ export const fetchParams =
   "relativehumidity_2m," +
   "apparent_temperature," +
   "precipitation," +
-  "freezing_level_height," +
-  "is_day";
+  "freezing_level_height";
 // 'cloudcover,' +
 
 export const fetchParamsOnlyCodes = "hourly=" + "weathercode," + "is_day";
